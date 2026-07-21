82230e0604e0de947b40bbea126263bb815caa1f
sweetakumari255 <aksweta91422@gmail.com>
2026-07-21 18:03:38 +0530

    Fix: TypeScript error for color parameter in ProductInteraction

--- apps/client/src/components/ProductInteraction.tsx
+++ apps/client/src/components/ProductInteraction.tsx
@@ -54,7 +54,7 @@ const ProductInteraction = ({
       <div className="flex flex-col gap-2 text-xs">
         <span className="text-gray-500">Size</span>
         <div className="flex items-center gap-2">
-          {product.sizes.map((size) => (
+          {product.sizes.map((size: string) => (
             <div
               className={`cursor-pointer border-1 p-[2px] ${
                 selectedSize === size ? "border-gray-600" : "border-gray-300"
@@ -79,7 +79,7 @@ const ProductInteraction = ({
       <div className="flex flex-col gap-2 text-sm">
         <span className="text-gray-500">Color</span>
         <div className="flex items-center gap-2">
-          {product.colors.map((color) => (
+          {product.colors.map((color:string) => (
             <div
               className={`cursor-pointer border-1 p-[2px] ${
                 selectedColor === color ? "border-gray-300" : "border-white"
