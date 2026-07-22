a6034a6df00cfa5af953a100fd5442fffbe4cd2e
sweetakumari255 <aksweta91422@gmail.com>
2026-07-22 11:29:44 +0530

    Force dynamic rendering globally to fix useSearchParams build errors

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+
+export const dynamic = "force-dynamic";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
